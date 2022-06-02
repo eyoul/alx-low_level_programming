07a2e5bde070735bb4fe285c583b85768989c1e6
eyoul <eyoulshimeles2@gmail.com>
2022-06-02 15:55:09 -0700

    Tash8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}

febee1dd549f84052f8bf2a1a8259c13bbd80ae3
eyoul <eyoulshimeles2@gmail.com>
2022-06-02 16:34:27 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 0);
 
 	return (1);
 }

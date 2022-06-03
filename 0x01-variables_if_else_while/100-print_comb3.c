1c54f4eda4abebba61a7a3e6dedeffeb1acdff5f
eyoul <eyoulshimeles2@gmail.com>
2022-06-03 15:56:49 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+int c, i;
+
+c = i = '0';
+
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+putchar(c);
+putchar(i);
+
+if ((c != '9') || (c == '9' && i != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}

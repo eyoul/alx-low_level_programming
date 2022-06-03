a30ca8747ee209927c110ff218ec17119131abd9
eyoul <eyoulshimeles2@gmail.com>
2022-06-03 12:07:26 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n > 0)
 {
-printf("%d is positive\n", n);
+printf("%i is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative\n", n);
+printf("%i is negative\n", n);
 }
 else
 {
-printf("%d is zero\n");
+printf("is zero\n");
 }
 return (0);
 }

3f0729a8300cdd07e45d716b1262640829882636
eyoul <eyoulshimeles2@gmail.com>
2022-06-03 16:13:50 -0700

    Task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,20 @@ int main(void)
 {
 int c, i;
 
-c = i = '0';
-
-for (c = '0'; c <= '8'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
+{
+if (c < i)
 {
 putchar(c);
 putchar(i);
 
-if ((c != '9') || (c == '9' && i != '9'))
+if ((c != '8') || (c == '8' && i != '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }

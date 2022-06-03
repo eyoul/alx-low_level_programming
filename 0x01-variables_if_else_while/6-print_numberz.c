071f77eacfeee06bc961fde328c1849f5747c500
eyoul <eyoulshimeles2@gmail.com>
2022-06-03 14:59:29 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+
+putchar(c);
+
+putchar('\n');
+return (0);
+}

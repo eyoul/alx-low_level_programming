1c8279fea391046d8ff1b2139a3474beffeb11f8
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 15:53:00 -0700

    Repo

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+/*
+*File: 11-print_to_98.c
+*Auth: Eyoul Shimeles
+*/
+
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
+void print_times_table(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}

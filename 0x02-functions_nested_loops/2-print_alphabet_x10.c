de65b0a5fe3962bf2560d1e587608a5e91ba09b8
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 12:36:27 -0700

    Repo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
 
 /**
  * Write a function that prints 10 times the alphabet, in lowercase, followed by a new line
- */
- void print_alphabet_x10(void);
- {
-     int coun = 0;
-     char alpha;
+*/
+void print_alphabet_x10(void)
+{
+	int coun = 0;
+	char alpha;
 
-     while(coun++ <= 9)
-     {
-         for (alpha = 97; alpha <= 122; alpha++)
-            _putchar(alpha);
-         _putchar('\n');
-     }
- }
+	while(coun++ <= 9)
+	{
+		for (alpha = 97; alpha <= 122; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}

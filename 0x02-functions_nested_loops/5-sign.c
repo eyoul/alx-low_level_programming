276a68738b56e8f892f255d7d013d1a6851e3a7f
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 14:45:43 -0700

    Repo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,10 +6,10 @@
 #include "main.h"
 /**
 *print_sign -prints the sign of a number.
-*@c:The character to be checked. 
-*Return:1 and prints + if n is greater than zero.
-*Return:0 and prints 0 if n is zero.
-*Return:-1 and prints - if n is less than zero
+*@c: The character to be checked. 
+*Return: 1 and prints + if n is greater than zero.
+*Return: 0 and prints 0 if n is zero.
+*Return: -1 and prints - if n is less than zero
 */
 int print_sign(int n)
 {
@@ -27,5 +27,5 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		return (-1);
-	}   
+	}
 }

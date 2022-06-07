2ef4f2325353874f88080e806900c1f07427bd22
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 16:38:56 -0700

    Repo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,10 +15,10 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
+			_putchar("%d, ", n);
 	}
 	else
 		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-	printf("98\n");
+			 _putchar("%d, ", n);
+	 _putchar("98\n");
 }

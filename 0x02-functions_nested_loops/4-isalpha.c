6e5f9057081da7f29d67b2bf972042603a5a68fe
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 14:01:29 -0700

    Repo

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,7 +13,7 @@
 
 int _isalpha(int c)
 {
-    if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+    if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
         return (1);
     else
         return (0);

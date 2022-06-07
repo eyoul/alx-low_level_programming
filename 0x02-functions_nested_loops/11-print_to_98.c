8ff2538c7935314ece1b8ab9b4c8f1065cd33972
eyoul <eyoulshimeles2@gmail.com>
2022-06-07 16:33:01 -0700

    Repo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
  *               in order separated by a comma followed by a space.
  * @n: The number to begin counting at.
  */
-void print_times_table(int n)
+void print_to_98(int n)
 {
         if (n >= 98)
         {

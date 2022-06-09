a7fd26b79a158d353fab82a7fe33ede00ffbb944
eyoul <eyoulshimeles2@gmail.com>
2022-06-09 16:18:49 -0700

    Repo

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints
+*print_most_numbers - prints
 *
-* Return: void
+*Return: void
 */
 void print_most_numbers(void)
 {
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c != '4'))
+	if (c != '2' || c != '4')
 	_putchar(c);
 	}
 	_putchar('\n');

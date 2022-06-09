55dff8ed3cd33754a8c9ce8a71b46c4094e85d86
eyoul <eyoulshimeles2@gmail.com>
2022-06-09 11:32:21 -0700

    Repo

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,21 +9,20 @@
  */
 
 int largest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
 {
-	int largest;
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-	if ((a > b) && (a > c))
-	{
-		largest = a;
-	}
-	if ((b > a) && (b > c))
-	{
-		largest = b;
-	}
-	if((c > a) && (c > b))
-	{
-		largest = c;
-	}
-
-	return (largest);
+return (largest);
 }

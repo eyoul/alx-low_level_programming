7d4f3d1f8b1d18d97729f5dd99eca2b9391c248f
eyoul <eyoulshimeles2@gmail.com>
2022-06-09 15:03:02 -0700

    Repo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _putchar - putchar 0123456789
+ * @c: char
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+}

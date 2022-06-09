5144dd482ad12a37106500197077b5ef73b668a3
eyoul <eyoulshimeles2@gmail.com>
2022-06-09 13:58:13 -0700

    Repo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+int i;
+	if (i >= 'A' && <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}

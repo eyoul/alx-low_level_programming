afe40ca880385b346a0855e07a9fdd7a141ac0bd
eyoul <eyoulshimeles2@gmail.com>
2022-06-09 14:05:45 -0700

    Repo

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+
+}
+

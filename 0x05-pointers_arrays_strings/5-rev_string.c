b5fd87be358f90c7618ab154496d1ad07591add1
eyoul <eyoulshimeles2@gmail.com>
2022-06-13 16:42:14 -0700

    Repo

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+# include "main.h"
+
+/**
+ * rev_string - function reverses a string
+ * @s: loohcS yM
+ */
+void rev_string(char *s)
+{
+	int x;
+	int y;
+	int tempo;
+	int len;
+
+	while (s[x] != 0)
+	{
+		x++;
+	}
+	len = x;
+	x = 0;
+	y = len - 1;
+
+	while (i < j)
+	{
+		tempo = s[x];
+		s[x] = s[y];
+		s[y] = tempo;
+		x++;
+		y--;
+	}
+}

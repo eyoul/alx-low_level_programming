4fc16a3c2d2c4aff0a1050063c39b256ac266473
eyoul <eyoulshimeles2@gmail.com>
2022-06-15 14:32:06 -0700

    Repo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcat -  concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: The destination string
  * @src: The source string
  *

bdb82a3005635b0f8593e9ecb6c88ef103e23184
eyoul <eyoulshimeles2@gmail.com>
2022-06-16 15:03:14 -0700

    Repo

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - function with one argument
+ * @n: int type argument
+ *
+ * Description: prints an integer
+ * Return: value of n
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}

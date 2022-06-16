b7a12241abb94cffff0b7238093a715746c58e50
eyoul <eyoulshimeles2@gmail.com>
2022-06-16 13:39:39 -0700

    Repo

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,25 +3,25 @@
 /**
   * string_toupper - Changes all lowercase letters to uppercase
   *
-  * @str: String to return
+  * @p: String to return
   *
   * Return: String
   */
 
-	char *string_toupper(char *)
+	char *string_toupper(char *p)
 	{
 	
 		int i;
 
-		for (i = 0;str[i] != '\0'; i++)
+		for (i = 0;p[i] != '\0'; i++)
 		{
 		
-			if (str[i] >= 97 && str[i] <= 122)
+			if (p[i] >= 97 && p[i] <= 122)
 			{
-				str[i] = str[i] - 32;
+				p[i] = p[i] - 32;
 			}
 
 	}
 
-	return (str);
+	return (p);
 }

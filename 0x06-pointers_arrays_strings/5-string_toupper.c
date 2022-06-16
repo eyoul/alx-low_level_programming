7e760e9b748b15496e4b5119495b340b5af29ed6
eyoul <eyoulshimeles2@gmail.com>
2022-06-16 13:42:46 -0700

    Repo

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,12 @@
 
 	char *string_toupper(char *p)
 	{
-	
+
 		int i;
 
-		for (i = 0;p[i] != '\0'; i++)
+		for (i = 0; p[i] != '\0'; i++)
 		{
-		
+
 			if (p[i] >= 97 && p[i] <= 122)
 			{
 				p[i] = p[i] - 32;

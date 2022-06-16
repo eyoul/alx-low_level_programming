8223f163e999e7d9b99189df0921fbea3ca8d039
eyoul <eyoulshimeles2@gmail.com>
2022-06-16 03:54:16 -0700

    Repo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,12 @@ char *_strcat(char *dest, char *src)
 	while (*dest != '\0')
 	{
 		dest++;
-	}
-
-	while (*src != '\0')
-	{
+		while (*src != '\0')
+		{
 		*dest = *src;
 		dest++;
 		src++;
+		}
 	}
 	*dest = '\0';
 	return (s);

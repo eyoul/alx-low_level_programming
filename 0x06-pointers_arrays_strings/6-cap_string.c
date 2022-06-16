a5a952d9c3e219b0c0e4aa7fd7e3a97a893d77af
eyoul <eyoulshimeles2@gmail.com>
2022-06-16 14:08:33 -0700

    Repo

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @p: capit ...
+ *
+ * Return: the char value
+ */
+char *cap_string(char *p)
+{
+	int a = 0, i;
+	int b = 13;
+	char c[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+		while (i < b)
+		{
+		if ((a == 0 || p[a - 1] == c[i]) && (p[a] >= 97 && p[a] <= 122))
+			p[a] -= 32
+		i++;
+		}
+	a++;
+	}
+	return (p);
+}

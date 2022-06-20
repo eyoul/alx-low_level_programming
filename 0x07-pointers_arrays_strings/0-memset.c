5f2e71db802847bf75fd99650190a98d68815d82
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 10:16:41 -0700

    Repo

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,12 +14,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
-		i++;
 	}
 	return (s);
 }

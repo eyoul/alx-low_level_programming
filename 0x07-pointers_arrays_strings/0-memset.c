4138ce8585b596f97ad95a62a5ce059eca7a4da9
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 10:37:36 -0700

    Repo

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,7 +16,6 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-
 	while (i < n)
 	{
 	s[i] = b;
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,4 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 
-#endif /*MAIN_H
+#endif /*MAIN_H*/

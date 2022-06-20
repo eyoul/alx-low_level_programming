9858b82bd6a38091984e86dd4b08ad10c139bb8e
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 12:02:28 -0700

    Repo

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strchr - locates a character in a string
  *
@@ -21,5 +21,6 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 		return (s);
+
 	return (NULL);
 }

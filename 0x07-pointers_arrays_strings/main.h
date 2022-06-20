bd27bf1210b0586b9649391850868174f893a47e
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 14:35:54 -0700

    Repo

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -10,4 +14,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+#endif/*MAIN_H*/

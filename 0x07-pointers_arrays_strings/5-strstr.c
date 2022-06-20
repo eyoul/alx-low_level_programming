ce90dc133bc3bd3b3a5978d587adfdfb454da44b
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 13:57:09 -0700

    Repo

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int s = 0, i;
+	int i;
+	int s = 0;
 
 	while (needle[s] != '\0')
 		s++;
+
 	while (*haystack)
 	{
 		for (i = 0; needle[i]; i++)
 		{
 			if (heystack[i] != needle[i])
+
 				break;
 		}
 		if (i != s)

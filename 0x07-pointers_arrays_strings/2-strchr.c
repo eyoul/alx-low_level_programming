82fc70d0466ae008e20cc6fd4183239d462c137f
eyoul <eyoulshimeles2@gmail.com>
2022-06-20 13:10:51 -0700

    Repo

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _strchr - locates a character in a string
  *
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);

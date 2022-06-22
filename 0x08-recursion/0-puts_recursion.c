01da8b8c8aae5eae4925ebff74572ab463588fbf
eyoul <eyoulshimeles2@gmail.com>
2022-06-22 12:05:51 -0700

    Repo

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion : a function that prints a string
+ *
+ * Return: the 0 value 
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}

7cd6585eb970500573a3bbd37c8083237703e13b
eyoul <eyoulshimeles2@gmail.com>
2022-06-24 13:42:02 -0700

    Repo

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+	}
+		return (0);
+}

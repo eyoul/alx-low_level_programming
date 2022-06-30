32a614192ecbe0df4759b98e8f5c786c160f504a
eyoul <eyoulshimeles2@gmail.com>
2022-06-30 15:06:42 -0700

    Repo

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);

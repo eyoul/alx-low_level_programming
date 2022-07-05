5f4ec41ed06524e4dfc480123aff3c262dfcb060
eyoul <eyoulshimeles2@gmail.com>
2022-07-05 05:27:06 -0700

    Repo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef HEADER
-#define HEADER
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - dog attributes
@@ -23,4 +23,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif

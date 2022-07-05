1db2e0adef1a2ba9f903406064d53d754cc26f45
eyoul <eyoulshimeles2@gmail.com>
2022-07-05 05:17:35 -0700

    Repo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * stract dog - dog attributes
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner:the owner of the dog
+ *
+ * description: the info of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - type of the dog stract
+ */
+typedef stract dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif

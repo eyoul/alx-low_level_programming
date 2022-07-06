f5994f26b8e15ccc459e4067b6667380c9454cc5
eyoul <eyoulshimeles2@gmail.com>
2022-07-06 11:17:02 -0700

    Repo

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,10 @@
 
 /**
  * print_name - a function that prints a name.
- *
  * @name: the name given
  * @f: function of name
  *
  * Rteurn: void
- *
  */
 void print_name(char *name, void (*f)(char *));
 {

eedffe13aa8362b360924681fef94b4baa0540b5
eyoul <eyoulshimeles2@gmail.com>
2022-07-06 11:50:20 -0700

    Repo

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - a function given as a parameter on each element of an array.
+ * array_iterator - a function given as a
+ * parameter on each element of an array.
  *
  * @array: array to execute func on
  * @size: is the size of the array

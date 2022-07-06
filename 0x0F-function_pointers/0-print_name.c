d5bae2c954e9d79139e28f3c265053c4da2abf3c
eyoul <eyoulshimeles2@gmail.com>
2022-07-06 11:26:24 -0700

    Repo

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,9 +7,9 @@
  *
  * Rteurn: void
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
-		return;
-		(*f)(name);
+	if (name != NULL && f != NULL)
+
+		f(name);
 }

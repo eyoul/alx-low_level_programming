aa6cc64fe3845221da7722ea3b8b609c54da1c00
eyoul <eyoulshimeles2@gmail.com>
2022-07-06 11:20:47 -0700

    Repo

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_
 
 #include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

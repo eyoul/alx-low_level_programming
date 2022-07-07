adbe8a7a3e876c9660951219e4247ba8484cd51e
eyoul <eyoulshimeles2@gmail.com>
2022-07-07 13:29:00 -0700

    Repo

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,10 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+void print_s(va_list s);
+void print_f(va_list f);
+void print_i(va_list i);
+void print_c(va_list c);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

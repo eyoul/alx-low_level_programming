b353b5e0e47d7475f2c37b08fd5813c2ef2981ee
eyoul <eyoulshimeles2@gmail.com>
2022-07-07 10:50:47 -0700

    Repo

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,9 +2,9 @@
 
 /**
  * sum_them_all - the sum of all its parameters.
- * 
+ *
  * @n: number of parameters
- * 
+ *
  * Return: sum of all all number
  */
 
@@ -17,8 +17,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
-	va_end (ap);
+		sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 
 }

a2cebc815b4960ce1611564f4469aca5484ee856
eyoul <eyoulshimeles2@gmail.com>
2022-07-07 11:35:26 -0700

    Repo

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(pa, int);
-		printf("%d\n", num);
+		printf("%d", num);
 		if (i < n - 1 && separator)
-			printf("%s\n", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(pa);

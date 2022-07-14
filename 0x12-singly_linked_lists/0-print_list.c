151f6601f3823b96e2cb7011e26d36ed25ada84a
eyoul <eyoulshimeles2@gmail.com>
2022-07-14 14:39:48 -0700

    Repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+	size_t nodes = 0;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
+	while (h)
 	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
-		tmp = tmp->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n" h->len,h-str);
+		nodes++;
+		h = h->next;
 	}
-	return (i);
+
+	return (nodes);
+
 }

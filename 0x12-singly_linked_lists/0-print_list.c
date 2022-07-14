fcbcb4f659066dba9d89e8653b1c8b1e157913f6
eyoul <eyoulshimeles2@gmail.com>
2022-07-14 14:42:32 -0700

    repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n" h->len,h-str);
+			printf("[%d] %s\n", h->len, h->str);
 		nodes++;
 		h = h->next;
 	}

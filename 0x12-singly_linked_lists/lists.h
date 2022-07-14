b17c0541389fb51206a62546b14cfa51a69173e5
eyoul <eyoulshimeles2@gmail.com>
2022-07-14 14:06:30 -0700

    Repp:

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
+ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,8 +18,12 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif /*LIST_H*/
+
+#endif /* _LISTS_H_ */

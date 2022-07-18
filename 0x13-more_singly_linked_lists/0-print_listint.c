426d934d1e75c09f5f5b9bd8dc045081daaa3b39
eyoul <eyoulshimeles2@gmail.com>
2022-07-18 13:51:00 -0700

    Repo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,9 +13,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	for (h != NULL; h = h->next; count++)
+	while (h != NULL)
 	{
+		h = h->next;
 		printf("%d\n", h->n);
+		count++
 	}
 	return (count);
 }
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,2 @@
+#include
+listint_t *add_nodeint(listint_t **head, const int n)

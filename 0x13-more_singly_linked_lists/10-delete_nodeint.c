73399281c407a600296289870cb91e6816c1a199
eyoul <eyoulshimeles2@gmail.com>
2022-07-19 04:23:44 -0700

    Repo

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,8 +22,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = next;
 		return (1);
 	}
+	temp = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+	}
 	next = temp->next;
 	temp->next = next->next;
 	free(next);
 	return (1);
+
 }

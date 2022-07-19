965bac930b90d7935e2369e71491e19660704489
eyoul <eyoulshimeles2@gmail.com>
2022-07-19 00:28:21 -0700

    Repo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,7 +17,7 @@ size_t print_listint(const listint_t *h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++
+		count++;
 	}
 	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -30,4 +32,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+
 #endif /*LISTS_H*/

d52256a55ffc277f3d9ab8fb50a669915fd4c59d
eyoul <49184150+eyoul@users.noreply.github.com>
2022-07-21 22:35:25 +0300

    Repo

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * 
+ * Return: 0 if big endian, 1 if little endian 
+ */
+int get_endianness(void)
+{
+	int i;
+	char *c;
+
+	i = 1;
+	c = (char *)&i;
+	return (*c);
+}

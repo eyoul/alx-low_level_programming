b30d1e8c8c8e4ffe1bf356041c69e3593d9457bf
eyoul <49184150+eyoul@users.noreply.github.com>
2022-07-21 21:07:31 +0300

    Repo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int j;
+	int i;
+	unsigned int j;
 
-    j = 0;
-    if (!b)
-        return (0);
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
-    }
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        j <<= 1;
-        if (b[i] == '1')
-            j += 1;
-    }
-    return (j);
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }

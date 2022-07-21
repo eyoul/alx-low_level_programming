2548b45fe32cdc4ae765536d5b886368ad9d134b
eyoul <49184150+eyoul@users.noreply.github.com>
2022-07-21 21:09:34 +0300

    Repo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint -converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars
- * 
+ *
  * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)

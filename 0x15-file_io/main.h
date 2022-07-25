1a8f3a817c52ed50a0cd2ff485412ecb3b30f8f4
eyoul <eyoulshimeles2@gmail.com>
2022-07-25 13:38:24 -0700

    repo

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

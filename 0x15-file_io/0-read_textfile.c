ced8d4ef80c56a6877f8c83678041a37df991e83
eyoul <eyoulshimeles2@gmail.com>
2022-07-25 13:33:22 -0700

    Repo

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	close(o);
-
-	return (w);
-}
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+{

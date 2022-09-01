aae806f262612441b68de73156ca8625fc542395
eyoul <eyoulshimeles2@gmail.com>
2022-09-01 11:28:02 -0700

    Repo

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,11 +12,8 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n;
 
-<<<<<<< HEAD
+
 	for (n = 0; h!= NULL; n++)
-=======
-	for (i = 0; h != NULL; n++)
->>>>>>> b69e464677cb27f307d59e250a71b44ba8c806bf
 		h = h->next;
 	return (n);
 }

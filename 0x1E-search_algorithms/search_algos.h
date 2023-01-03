7e0bba4e10bea22c4387ab4691fe95fe76feddbb
eyoul <eyoulshimeles2@gmail.com>
2023-01-03 00:52:10 -0800

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,8 +13,8 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
+
 typedef struct listint_s
 {
 		int n;
@@ -31,8 +31,8 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
+
 typedef struct skiplist_s
 {
 				int n;
